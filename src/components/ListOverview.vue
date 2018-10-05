fd5866984d96952f3829613abbcc0509bdc54ded
Mike <mikew7224@gmail.com>
2018-10-04 20:36:49 -0500

    Created the list overview layout, and created the component and form.
    git push origin list_overview
    
    i

---
+++ src/components/ListOverview.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="listOverview">
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-toolbar color="cyan"
+          dark
+          >
+          <v-toolbar-side-icon>
+        </v-toolbar-side-icon>
+            <v-toolbar-title>Movies</v-toolbar-title>
+
+
+
+            <v-btn icon>
+              <v-icon></v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon></v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-list three-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+            >
+              {{ item.header }}
+            </v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :inset="item.inset"
+              :key="index"
+            ></v-divider>
+
+            <v-list-tile
+              v-else
+              :key="item.title"
+              avatar
+              @click=""
+            >
+              <v-list-tile-avatar>
+                <img :src="item.avatar">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+          <v-btn justify center
+          dark>
+          Add New Movie</v-btn>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            avatar: 'https://m.media-amazon.com/images/M/MV5BMTg5NTA3NTg4NF5BMl5BanBnXkFtZTcwNTA0NDYzOA@@._V1_SY1000_CR0,0,675,1000_AL_.jpg',
+            title: 'Looper',
+            subtitle: "Movie Info"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg',
+            title: 'Avatar',
+            subtitle: "Movie Info"
+          },
+          { divider: true, inset: true },
+          {
+            avatar: 'https://m.media-amazon.com/images/M/MV5BMjI0MDMzNTQ0M15BMl5BanBnXkFtZTgwMTM5NzM3NDM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',
+            title: 'A Quiet Place',
+            subtitle: "Movie Info"
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style scoped>
---
+++ src/views/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='form'>
+    <ListOverview></ListOverview>
+  </div>
+</template>
+
+<script>
+import ListOverview from '@/components/ListOverview.vue'
+
+export default {
+  name: 'form',
+  components: {
+    ListOverview,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
