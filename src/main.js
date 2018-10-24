294288c4cc45c0fc698b9301568d21a6052d994d
tctcodes <timthom64@gmail.com>
2018-10-23 19:58:19 -0500

    added vuelidate to project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
