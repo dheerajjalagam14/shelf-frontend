c9eabd4677d9ce7c4e4f02831e05c7e58d60197c
tctcodes <timthom64@gmail.com>
2018-11-17 15:41:31 -0600

    created vuex module for login

---
+++ src/store/modules/login.js
@@ -0,0 +1,26 @@
+import api from '../../api/shelfuserapi';
+
+export const initialState = {
+
+};
+
+export const getters = {
+
+};
+
+export const mutations = {
+
+};
+
+export const actions = {
+
+};
+
+// Exports
+export default {
+    namespaced: true,
+    state: { ...initialState },
+    getters,
+    mutations,
+    actions,
+  }
