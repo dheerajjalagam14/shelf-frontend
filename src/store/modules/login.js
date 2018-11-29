17b4235fce3c30efa67a52d715745fff23cb9eb6
tctcodes <timthom64@gmail.com>
2018-11-28 18:42:06 -0600

    stashing login work

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -1,32 +1,47 @@
-// import api from '../../api/shelfuserapi.js';
+import api from '../../api/shelfuserapi.js';
+import VueRouter from 'vue-router';
 
 export const UPDATE_LOGIN = 'UPDATE_LOGIN';
 
 export const initialState = {
   loggedIn: false,
+  userToken: null,
+  loginPending: false,
+  loginError: false,
 };
 
 export const getters = {
-
+  getUserToken: state => state.userToken,
 };
 
 export const mutations = {
-  [UPDATE_LOGIN](state, value) {
-    state.loggedIn = value;
+  [UPDATE_LOGIN](state, result) {
+    if(result.token === undefined) {
+      state.loginPending = false;
+      state.loginError = true;
+    } else {
+      state.userToken = result.token;
+      state.loggedIn = true;
+      state.loginError = false;
+      state.loginPending = false;
+      this.$router.push('/');
+    }
   },
 };
 
 export const actions = {
-  attemptLogin: ({commit}, payload) =>{
-    // api.login(payload);
+  attemptLogin: ({state, commit}, payload) =>{
+    state.loginPending = true;
+    state.loginError = false;
+    commit(UPDATE_LOGIN, api.login(payload));
   }
 };
 
 // Exports
 export default {
-    namespaced: true,
-    state: { ...initialState },
-    getters,
-    mutations,
-    actions,
-  }
+  namespaced: true,
+  state: { ...initialState },
+  getters,
+  mutations,
+  actions,
+}
